44fea95f6dc25bc049f94043f6a8d6e4f3726618
tetostois <tetakamgasteve@gmail.com>
2026-04-16 09:23:26 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const projectRoot = fileURLToPath(new URL('.', import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
+  root: projectRoot,
   plugins: [vue()],
   resolve: {
     alias: {
